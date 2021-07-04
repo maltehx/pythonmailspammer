eba2d48762065652ab1a7d04f9a90426ecef558c
maltehx <mvlte.hxrms@gmail.com>
2021-07-05 00:22:20 +0200

    input update

--- main.py
+++ main.py
@@ -3,26 +3,22 @@ import time
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-value = 0
-username = ""
-password = ""
-mail_sender = ""
-mail_receiver = ""
-mail_subj = ""
-mail_text = ""
-
-print("Mail spammer  -  github/maltehx")
-print("Mail Username: ")
-input(username)
-print("Mail password:")
-input(password)
-
+value = 0;
 username = ""
 password = ""
 mail_from = ""
 mail_to = ""
 mail_subject = ""
 mail_body = ""
+
+print("Mail spammer  -  github/maltehx")
+username = input("Your email: ")
+password = input("Your password: ")
+mail_from = input("Sender email: ")
+mail_to = input("Mail receiver: ")
+mail_subject = input("Mail subject: ")
+mail_body = input("Mail Text: ")
+value = input("How many mails do you want to send? ")
 
 mimemsg = MIMEMultipart()
 mimemsg['From'] = mail_from
@@ -31,8 +27,8 @@ mimemsg['Subject'] = mail_subject
 mimemsg.attach(MIMEText(mail_body, 'plain'))
 connection = smtplib.SMTP(host='smtp.gmail.com', port=587)
 connection.starttls()
-connection.login(username, "")
-for x in range(value):
+connection.login(username, password)
+for x in range(int(value)):
     connection.send_message(mimemsg)
     print("sent!")
     time.sleep(2)
